95abeac6894a91f43c95188503c1aa6a3d3cb417
23CABSAKTHIRENGANATHANk <139529312+23CABSAKTHIRENGANATHANk@users.noreply.github.com>
2024-12-12 13:07:50 +0000

    pg 1 compleat

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 class Test {
-  public static void main(String args[]) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter integer value:");
-    int a = sc.nextInt();
-    System.out.println("integer value: " + a);
-    sc.close(); // Ensure resources are released
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); // Scanner for user input
+    System.out.println("Enter integer value:"); // Prompt for input
+    int a = sc.nextInt(); // Read integer input
+    System.out.println("integer value: " + a); // Output the entered value
+    sc.close(); // Close the scanner to release resources
   }
 }
