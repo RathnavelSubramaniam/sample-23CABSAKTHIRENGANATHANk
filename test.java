36d233db0f408ec8e415cd8f461ba2b8d05bf356
23CABSAKTHIRENGANATHANk <139529312+23CABSAKTHIRENGANATHANk@users.noreply.github.com>
2024-12-12 13:01:24 +0000

    pg 1 commit

--- test.java
+++ test.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-class test 
-{
-  public static void main(String args[]) 
-  {
-    Scanner sc=new Scanner(System.in);
+class Test {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
     System.out.println("Enter integer value:");
-    int a=sc.nextInt();
-    System.out.println("integer value:"+a);
+    int a = sc.nextInt();
+    System.out.println("integer value:" + a);
+    sc.close(); // Ensure resources are released
   }
 }
